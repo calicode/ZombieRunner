333c5a9610bd44246861c282c58f214d71d461e5
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-08-23 20:52:33 -0500

    Added navmesh. Zombie that follows player. Water table. Player spawn points

---
+++ ZombieRunner/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    GameObject playerSpawnPoints;
+    // Use this for initialization
+    void Start()
+    {
+        playerSpawnPoints = GameObject.FindGameObjectWithTag("PlayerSpawnPoints");
+
+    }
+
+    // Update is called once per frame
+
+    Transform pickRandomSpawn()
+    {
+        Debug.Log("Childcount is" + playerSpawnPoints.transform.childCount);
+
+        return playerSpawnPoints.transform.GetChild(Random.Range(0, playerSpawnPoints.transform.childCount));
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            transform.position = pickRandomSpawn().transform.position;
+
+        }
+    }
+}
